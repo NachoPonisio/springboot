082c198148543e12b620f54524d9ca85b76dc80e
NachoPonisio <ponisio.ignacio@gmail.com>
2018-07-13 00:34:34 -0300

    Se generan repositories y se implementa applicationListener

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -28,6 +28,11 @@ public class Author {
         this.books = books;
     }
 
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
     public String getFirstName() {
         return firstName;
     }
